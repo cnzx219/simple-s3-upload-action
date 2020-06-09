146374b54cf208104f9db1a7239cf78fa7ebe90d
Youngchan Je <ochanje210@gmail.com>
2020-06-09 11:56:47 +0900

    feat: customizable destination dir

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const core = require('@actions/core');
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 const path = require('path');
-const shortid = require('shortid');
 const klawSync = require('klaw-sync');
 const { lookup } = require('mime-types');
 
-const AWS_KEY_ID = core.getInput('aws_key_id', {
+const AWS_KEY_ID = core.getInput('AWS_ACCESS_KEY_ID', {
   required: true
 });
-const SECRET_ACCESS_KEY = core.getInput('aws_secret_access_key', {
+const SECRET_ACCESS_KEY = core.getInput('AWS_SECRET_ACCESS_KEY', {
   required: true
 });
-const BUCKET = core.getInput('aws_bucket', {
+const BUCKET = core.getInput('AWS_S3_BUCKET', {
   required: true
 });
-const SOURCE_DIR = core.getInput('source_dir', {
+const SOURCE_DIR = core.getInput('SOURCE_DIR', {
   required: true
 });
+const DEST_DIR = core.getInput('DEST_DIR', {
+  required: true
+})
 
 const s3 = new S3({
   accessKeyId: AWS_KEY_ID,
   secretAccessKey: SECRET_ACCESS_KEY
 });
-const objKey = shortid();
+const objKey = DEST_DIR;
 const paths = klawSync(SOURCE_DIR, {
   nodir: true
 });
@@ -57,9 +59,7 @@ function run() {
 
 run()
   .then(locations => {
-    core.info(`object key - ${objKey}`);
     core.info(`object locations - ${locations}`);
-    core.setOutput('object_key', objKey);
     core.setOutput('object_locations', locations);
   })
   .catch(err => {
